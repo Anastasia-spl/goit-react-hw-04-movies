449d7c8c181e8db087bf193eef81719ece60a2ea
AnastasiaPolkovnikova <68498874+AnastasiaPolkovnikova@users.noreply.github.com>
2021-04-27 12:52:21 +0800

    Update moviesApi.js

--- src/moviesApi.js
+++ src/moviesApi.js
@@ -1,23 +1,25 @@
 import axios from 'axios';
 import variables from './variables';
 
+axios.defaults.baseURL = 'https://api.themoviedb.org/3';
+  
 const requestedMoviesFetch  = async query => {
     return await axios
       .get(
-        `https://api.themoviedb.org/3/search/movie?api_key=${variables.ApiKey}&query=${query}`,
+        `/search/movie?api_key=${variables.ApiKey}&query=${query}`,
       ).then(response => response.data.results)
 };
   
 const movieDetailsFetch = async (movieId) => {
   return await axios
       .get(
-        `https://api.themoviedb.org/3/movie/${movieId}?api_key=${variables.ApiKey}`,
+        `/movie/${movieId}?api_key=${variables.ApiKey}`,
       ).then(response => response.data)
 }
   
 const popularMovieFetch = async () => {
  return await axios.get(
-      `https://api.themoviedb.org/3/trending/movie/day?api_key=${variables.ApiKey}`,
+      `/trending/movie/day?api_key=${variables.ApiKey}`,
     ).then(response => response.data.results);
 }
 
