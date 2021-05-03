6cb76a023f9007d41a40e6c74ec97b7928313ddc
AnastasiaPolkovnikova <68498874+AnastasiaPolkovnikova@users.noreply.github.com>
2021-05-03 10:01:21 +0800

    adds input focus

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,7 +1,10 @@
+import { useRef, useEffect } from 'react';
 import Button from '../Button';
 import styles from './SearchForm.module.scss';
 
 const SearchForm = ({ query, handleSubmit, handleChange }) => {
+  const inputRef = useRef();
+  useEffect(() => inputRef.current.focus(), []);
   return (
     <form className={styles.searchForm} onSubmit={handleSubmit}>
       <input
@@ -9,6 +12,7 @@ const SearchForm = ({ query, handleSubmit, handleChange }) => {
         type="text"
         onChange={handleChange}
         value={query}
+        ref={inputRef}
       />
       <Button type="submit">Search</Button>
     </form>
